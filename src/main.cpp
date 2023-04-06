a2cdb3d2cd15452d883b1ffcf358ff17b0da0c9c
Mihail Zhizhikin <mihailzizikin330@gmail.com>
2023-04-06 21:49:27 +0500

    Start.

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,20 @@ int main(void)
 
     /* Initialize the library */
     if (!glfwInit())
+    {
+        std::cout << "glfwInit failed" << std::endl;
         return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "Inspy", NULL, NULL);
     if (!window)
     {
+        std::cout << "glfwCreateWindow failed!" << std::endl;
         glfwTerminate();
         return -1;
     }
@@ -22,11 +30,11 @@ int main(void)
     glfwMakeContextCurrent(window);
 
     if (!gladLoadGL()) {
-        std::cout << "NEIN!";
+        std::cout << "Can't load glad";
         return -1;
     }
 
-    std::cout << "GL" << GLVersion.major << "." << GLVersion.minor << std::endl;
+    std::cout << "GL " << GLVersion.major << "." << GLVersion.minor << std::endl;
 
     glClearColor(0, 1, 0.5, 1);
 
@@ -38,6 +46,8 @@ int main(void)
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
+
+
 
         /* Poll for and process events */
         glfwPollEvents();
